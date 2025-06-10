a3cf760781e6ae688eefd4ac91ce9abb5f3dd603
yamoo9 <yamoo9@naver.com>
2025-06-10 14:19:33 +0900

    [학습] 변수 vs. 상수
    - let, var 변수
    - const 상수

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -20,6 +20,8 @@
 let $initialApples = 4 // camelCase
 console.log('$initialApples =', $initialApples)
 
+// ---------------------------------------------------------------
+
 // 잘못된 변수 이름
 // 공백(' ')은 안돼요!
 // let initial apples [❌]
@@ -48,5 +50,34 @@ console.log('addBuyApples =', addBuyApples) // undefined -> 27
 // let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수
 // console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) // 4 + 27
 
+// ---------------------------------------------------------------
 // let 변수 vs. var 변수
-// let 변수 vs. const 변수(상수)
+
+// 블록 스코프(block scope)
+// {
+//     var myName
+//     console.log(1, myName)
+
+//     myName = 'yamoo9'
+//     console.log(2, myName)
+// }
+
+// myName = 'y9'
+// console.log(3, myName)
+
+// {
+//     let myName2
+//     console.log(1, myName2)
+
+//     myName2 = 'yamoo9'
+//     console.log(2, myName2)
+// }
+
+// myName2 = 'y9'
+// console.log(3, myName2)
+
+
+// ---------------------------------------------------------------
+// let 변수 vs. const 변수(상수, constant)
+// - 변수 = 시간의 흐름에 따라 값이 변경됨
+// - 상수 = 처음 선언 시, 할당된 값이 이후 변경되지 않음
