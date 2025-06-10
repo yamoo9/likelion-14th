5714814caec27c6f7eeed7ca624d2f3f0ddd7d13
yamoo9 <yamoo9@naver.com>
2025-06-10 13:51:54 +0900

    [학습] JavaScript 변수(변할 수 있는)
    - 시간의 흐름에 따라 값이 변할 수 있음
    - 변수 선언
    - 선언된 변수에 값 할당

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -21,19 +21,32 @@ let $initialApples = 4 // camelCase
 console.log('$initialApples =', $initialApples)
 
 // 잘못된 변수 이름
-// 공백(' ')은 안되요!
+// 공백(' ')은 안돼요!
 // let initial apples [❌]
-// 첫글자가 숫자 안되요!
+// 첫글자가 숫자 안돼요!
 // let 4people = '4명의 친구들' [❌]
-// 하이픈(-) 기호 안되요!
+// 하이픈(-) 기호 안돼요!
 // let dark-impact = '다크 임팩트' [❌]
-// 예약어(예: if, for, while, function, ...) 안되요!
+// 예약어(예: if, for, while, function, ...) 안돼요!
 // let return = '회귀' [❌]
 
 // 추가로 구매한 사과의 갯수 = 27
 // let 추가로_구매한_사과의_갯수 = 27
+
+// 변수 선언 (값 할당 없이)
+let addBuyApples // 기본값 (undefined)
+// let addBuyApples = undefined (암묵적)
+console.log('addBuyApples =', addBuyApples) // undefined
+
+// 선언된 변수에 값 할당
+addBuyApples = 27
+console.log('addBuyApples =', addBuyApples) // undefined -> 27
+
 // console.log('추가로_구매한_사과의_갯수 = ', 추가로_구매한_사과의_갯수) // 27
 
 // 현재 내가 가진 사과의 갯수 = 처음 가진 사과의 갯수 + 추가로 구매한 사과의 갯수
 // let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수
 // console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) // 4 + 27
+
+// let 변수 vs. var 변수
+// let 변수 vs. const 변수(상수)
