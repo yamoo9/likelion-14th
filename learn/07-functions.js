46efc6abfacf709507948a2b5e27b218f1df32a6
yamoo9 <yamoo9@naver.com>
2025-06-10 16:24:55 +0900

    [실습] JavaScript 함수 매개변수
    - 함수이름 옆 소괄호() 안에 선언된 변수
    - 이런 변수를 특별해 매개변수라고 부름
    - 함수 안에서만 접근 가능(지역 변수 같은 느낌)
    - 함수 호출(실행)할 때 전달한 값이 매개변수 값으로 할당

--- learn/07-functions.js
+++ learn/07-functions.js
@@ -1,5 +1,7 @@
 // console.log('함수(Function)')
 
+// ------------------------------------------------------
+
 // 함수란?
 // 특정 순서에 따라 작업이 실행되는 코드 묶음(블록)
 
@@ -14,19 +16,23 @@
 // drawWater 함수 부르기(call)
 // drawWater 함수 r부르기(call)
 
+// ------------------------------------------------------
+
 // 함수 선언 (Function Declaration) 구문
 function drawWater() {
-    console.groupCollapsed('drawWater() 호출됨')
-        console.log('1. 빈 양동이 가져오기')
-        console.log('2. 양동이 들고 우물로 이동하기')
-        console.log('3. 우물에서 물 길어오기')
-        console.log('4. 집으로 귀가하기')
-    console.groupEnd()
+  console.groupCollapsed('drawWater() 호출됨')
+  console.log('1. 빈 양동이 가져오기')
+  console.log('2. 양동이 들고 우물로 이동하기')
+  console.log('3. 우물에서 물 길어오기')
+  console.log('4. 집으로 귀가하기')
+  console.groupEnd()
 }
+
+// ------------------------------------------------------
 
 // 함수 사용 (Call Function)
 // 재사용성 (Reusability)
-drawWater()
+// drawWater()
 
 // 동사형태
 // 함수를 "호출(실행)한다" 의미 - 약속된 규칙
@@ -44,3 +50,39 @@ drawWater()
 // function conti    nue() {}
 // function cla-----ss() {}
 // function for() {}
+
+// ------------------------------------------------------
+// 함수 매개변수 (Function Parameters)
+
+// 함수 밖에 있는 [ 전역 ] 변수 (global scope variables)
+// 코드 실행 범위 어디서나 접근이 가능한 변수 
+let year = 4027
+
+// 함수 선언
+function 일기쓰기(연도, 월, 일) {
+  // 함수이름 옆 () 안에 선언된 변수를 특별히 매개변수라 부른다.
+  // console.log(연도, 월, 일)
+
+  // 함수 내부에 선언된 변수
+  // 함수 안에서 사용
+  // 지역 변수 (local scope variables)
+
+  // 연도(year)
+  let year = 2025
+  // 월(month)
+  let month = 6
+  // 일(day)
+  let day = 10
+
+  // year = 2026
+
+  // 변수 + 문자 = 문장
+  // 변수('문자') + 변수('문자') = 문자 연결
+  console.log(연도 + '년 ' + 월 + '월 ' + 일 + '일에 일기를 쓴다.')
+}
+
+// 함수 호출(실행)
+일기쓰기(2025, 6, 10)
+일기쓰기(2025, 7, 1)
+// 일기쓰기(연도, 월, 일)
+// 일기쓰기(연도, 월, 일)
