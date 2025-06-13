660cfd203d309d13f7992caca64b635b62ef63ef
yamoo9 <yamoo9@naver.com>
2025-06-13 09:27:33 +0900

    [학습] 데이터 타입 검사
    - typeof 연산자
    - 객체 비교 (리마인드)

--- assets/scripts/learn/18-typeof-operator.js
+++ assets/scripts/learn/18-typeof-operator.js
@@ -3,11 +3,27 @@
 // --------------------------------------------------------------------------
 
 const o = {
-  name: 'object',
-  serialNumber: 20250613,
-  isObjectType: () => true,
-  isNotDefined: undefined,
-  empty: null,
-  symbol: Symbol('o'),
-  int: 100n,
+  // key(string): value(any),
+  'name': 'object', // string
+  'serialNumber': 20250613, // number
+  'isObjectType': () => true, // function -> boolean
+  'isNotDefined': undefined, // undefined
+  'empty': null, // null
+  'symbol': Symbol('o'), // symbol
+  'int': 100n, // bigint
 }
+
+// 어떤 데이터 타입인지 확인하고자 한다.
+// typeof 연산자 (typeof 연산자로 null과 object를 구분할 수 없다)
+// typeof 데이터 // 'string', 'number', 'boolean', ...
+
+console.log(typeof o)
+console.log(typeof o.name)
+console.log(typeof o.serialNumber)
+console.log(typeof o.isObjectType)
+console.log(typeof o.isObjectType())
+console.log(typeof o.isNotDefined)
+console.log(typeof o.empty)
+console.log(typeof o.symbol)
+console.log(typeof o.int)
+
