ace24982ce8e462065ff2e0847a5f832e5d60084
yamoo9 <yamoo9@naver.com>
2025-06-16 10:44:50 +0900

    [학습] DOM 요소 클래스 속성 이름 제어
    - Element.classList
    - .add()
    - .remove()
    - .item()
    - .toggle()
    - .contains()
    - .length

--- assets/scripts/learn/26-change-class-properties.js
+++ assets/scripts/learn/26-change-class-properties.js
@@ -1,14 +1,52 @@
 const exorcists = document.getElementById('exorcist-characters')
 
 const choi = exorcists.querySelector('.master-choi')
-// console.dir(choi.classList)
+const villain = exorcists.querySelector('[data-type="villain"]')
+
+
+// Element.classList -> DOMTokenList { add, remove, contains, item, replace, toggle }
+
+// let firstClassName = choi.classList.item(0)
+// let lastClassName = choi.classList.item(1)
+// console.log('firstClassName =', firstClassName)
+// console.log('lastClassName =', lastClassName)
+
+// 토글 전
+console.log('1. choi.classList.value의 아이템 갯수 =', choi.classList.length)
+
+// 클래스 이름 토글
+choi.classList.toggle('charactor-is-active')
+
+// 토글 후
+console.log('2. choi.classList.value의 아이템 갯수 =', choi.classList.length)
+
+// let activeClassName = 'charactor-is-active'
+// if (!choi.classList.contains(activeClassName)) {
+//   choi.classList.add(activeClassName)
+// } else {
+//   choi.classList.remove(activeClassName)
+// }
 
 // 오늘날 주로 사용하는 방법(NEW)
 {
-  choi.classList.add('active')
+  // choi.classList.add('active')
   // console.dir(choi.classList.value)
-  choi.classList.remove('master-choi')
+  // choi.classList.remove('master-choi')
 }
+
+{
+  // villain-ghost 항목 요소를 찾아서 클래스 이름을 여럿 추가한 뒤,
+  // 다시 클래스 이름을 여럿 제거해봅니다.
+  villain.classList.add('alpha', 'beta')
+  console.log(villain.className)
+  
+  villain.classList.remove('beta', 'character')
+  console.log(villain.className)
+  
+}
+
+
+
 
 // 이전 시대의 방법 (OLD)
 {
