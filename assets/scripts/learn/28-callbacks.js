a17860e190e32d914ca25ff027e5fb10b22c96c1
yamoo9 <yamoo9@naver.com>
2025-06-16 16:51:47 +0900

    [학습] 동기 코드는 다음 코드를 블로킹
    - 그래서 비동기 처리 필요

--- assets/scripts/learn/28-callbacks.js
+++ assets/scripts/learn/28-callbacks.js
@@ -12,21 +12,26 @@ const handleButtonToggleClass = () => {
   button.classList.toggle('is-active')
 }
 
+// 동기 방식은 코드 실행 완료될 때까지 다음 코드를 차단(블로킹)
+// 비동기 방식은 바로 실행되지 않고(다음 코드를 차단하지 않고) 나중에 코드가 호출(콜백)
+
 // 버튼 요소에 이벤트 리스너(함수) 추가
 // 콜백 = 다른 함수에 인수로 전달될 함수 (나중에 실행되도록)
 // 이벤트 = 언제 발생할 지 알 수 없음 (비동기 실행)
 button.addEventListener('click', handleBodyToggleClass)
 
+// 동기(sync) 실행 (블로킹: 코드 차단)
+handleBodyToggleClass() // 1 // 5s
+handleBodyToggleClass() // 2 // 5s
+handleBodyToggleClass() // 3 // 5s
+handleBodyToggleClass() // 4 // 5s
+handleBodyToggleClass() // 5 // 5s (25s)
+
+
 // addEventListener()를 사용해 리스너를 추가하는 것은 x N회 가능
 button.addEventListener('click', handleButtonToggleClass)
 
 
-// 동기(sync) 실행
-handleBodyToggleClass() // 1
-handleBodyToggleClass() // 2
-handleBodyToggleClass() // 3
-handleBodyToggleClass() // 4
-handleBodyToggleClass() // 5
 
 
 
