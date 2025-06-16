823dce2a4d60f5a68a572b0fe93829eb18ae617b
yamoo9 <yamoo9@naver.com>
2025-06-16 15:06:49 +0900

    [학습] 동기 vs. 비동기
    - 동기 처리 (실행 시점이 바로)
    - 비동기 처리 (실행 시점이 나중에)

--- assets/scripts/learn/28-callbacks.js
+++ assets/scripts/learn/28-callbacks.js
@@ -2,26 +2,46 @@
 // 전역 변수에 참조(왜? 객체 타입이니까)
 const button = document.querySelector('button.clickable')
 
+const handleBodyToggleClass = () => {
+  console.log('body')
+  document.body.classList.toggle('is-active')
+}
+
+const handleButtonToggleClass = () => {
+  console.log('button')
+  button.classList.toggle('is-active')
+}
+
 // 버튼 요소에 이벤트 리스너(함수) 추가
 // 콜백 = 다른 함수에 인수로 전달될 함수 (나중에 실행되도록)
-// 이벤트 = 언제 발생할 지 알 수 없음 (비동기)
-// button.addEventListener('click', () => {
-//   document.body.classList.toggle('is-active')
-// })
+// 이벤트 = 언제 발생할 지 알 수 없음 (비동기 실행)
+button.addEventListener('click', handleBodyToggleClass)
 
 // addEventListener()를 사용해 리스너를 추가하는 것은 x N회 가능
-// button.addEventListener('click', () => {
-//   button.classList.toggle('is-active')
-// })
+button.addEventListener('click', handleButtonToggleClass)
+
+
+// 동기(sync) 실행
+handleBodyToggleClass() // 1
+handleBodyToggleClass() // 2
+handleBodyToggleClass() // 3
+handleBodyToggleClass() // 4
+handleBodyToggleClass() // 5
+
+
 
 
 // 레거시 이벤트 vs. 모던 이벤트 모델
 // 변수에는 여러 값을 저장할 수 있다? 아니다 변수는 단 하나의 값만 저장할 수 있다?
 
-button.onclick = function() {
-  document.body.classList.toggle('is-active')
-}
+// button.onclick = function() {
+//   button.classList.toggle('is-active')
+// }
 
-button.onclick = function() {
-  button.classList.toggle('is-active')
-}
+// console.log(button.onclick)
+
+// button.onclick = function() {
+//   document.body.classList.toggle('is-active')
+// }
+
+// console.log(button.onclick)
