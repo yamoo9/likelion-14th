7ea7b7228360a3ae5f93e1361d25e7f95bb487d3
yamoo9 <yamoo9@naver.com>
2025-06-17 11:43:11 +0900

    [실습] Off Canvas 메뉴 구현 과정 4
    - 코드 정리
    - 네이티브 메서드 활용 (toggle)
    - 이벤트 리스너 분리

--- practices/01-off-canvas/finished/scripts/main.js
+++ practices/01-off-canvas/finished/scripts/main.js
@@ -8,13 +8,11 @@ const siteContainer = document.querySelector('.site-container')
 const toggleOffsiteButton = siteContainer.querySelector('button')
 const OFFSITE_OPEN_CLASSNAME = 'offsite-is-open'
 
-toggleOffsiteButton.addEventListener('click', () => {
+toggleOffsiteButton.addEventListener('click', handleToggleOffsite)
+
+function handleToggleOffsite() {
   // 2. 버튼을 클릭하면 화면을 오른쪽으로 밀어줍니다.
-  if (!body.classList.contains(OFFSITE_OPEN_CLASSNAME)) {
-    //    <body> 요소에 offsite-is-open 클래스 이름을 추가한다.
-    body.classList.add(OFFSITE_OPEN_CLASSNAME)
-  } else {
-    // 3. 버튼을 다시 클릭하면 화면을 왼쪽으로 밀어 넣습니다.
-    body.classList.remove(OFFSITE_OPEN_CLASSNAME)
-  }
-})
+  // 3. 버튼을 다시 클릭하면 화면을 왼쪽으로 밀어 넣습니다.
+  // 2 + 3 = 버튼을 클릭할 때마다 화면을 오른쪽 또는 왼쪽으로 이동시키는 토글을 진행합니다.
+  body.classList.toggle(OFFSITE_OPEN_CLASSNAME)
+}
