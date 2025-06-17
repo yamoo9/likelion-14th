3196be41c2ab984558961eeac8a38752f54bc384
yamoo9 <yamoo9@naver.com>
2025-06-17 14:42:19 +0900

    [실습] Modal Dialog 메뉴 구현 과정 3
    - 추후 이벤트 딥다이브 섹션에서 학습한 내용으로 해결 못한 문제를 확인

--- practices/02-modal-dialog/begin/scripts/main.js
+++ practices/02-modal-dialog/begin/scripts/main.js
@@ -1,11 +1,18 @@
 const body = document.body
 const modal_open_button = document.querySelector('main .modal-open-button')
-const modal_close_button = document.querySelector('.modal-overlay .modal-close-button')
+
+const modal_overlay = document.querySelector('.modal-overlay')
+const modal_close_button = modal_overlay.querySelector('.modal-close-button')
+
+
 
 modal_open_button.addEventListener('click', () => {
   body.classList.add('modal-is-open')
 })
 
-modal_close_button.addEventListener('click', () => {
+// modal_overlay.addEventListener('click', closeModalDialog)
+modal_close_button.addEventListener('click', closeModalDialog)
+
+function closeModalDialog() {
   body.classList.remove('modal-is-open')
-})
+}
