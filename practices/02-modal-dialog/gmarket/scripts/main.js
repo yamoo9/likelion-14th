b41d4004e7663dd4cb3e2d9f941fc452d7933e72
yamoo9 <yamoo9@naver.com>
2025-06-17 15:36:13 +0900

    [실습] Gmarket 개발 파트 2
    - 문서 객체 접근
    - 접근한 객체 이벤트 연결
    - 이벤트 리스너 추가
    - 사용자와 상호작용 기능 구현

--- practices/02-modal-dialog/gmarket/scripts/main.js
+++ practices/02-modal-dialog/gmarket/scripts/main.js
@@ -1,7 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
   // console.log('DOM Contents loaded')
   const dialog = document.querySelector('.dialog')
-  console.log(dialog) // HTMLFigureElement
+  // console.log(dialog) // HTMLFigureElement
+  const closeButton = dialog.querySelector('button')
+  // console.log(closeButton) // HTMLButtonElement
+  closeButton.addEventListener('click', () => {
+    document.body.classList.add('dialog-is-hidden')
+  })
 })
 
 // 코드 실행 시점 관련 -> load, DOMContentLoaded 이벤트 (변경)
