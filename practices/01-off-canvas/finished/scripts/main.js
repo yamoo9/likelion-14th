4096e176a0819288e6f5fe145af65fe453d6431d
yamoo9 <yamoo9@naver.com>
2025-06-17 11:40:51 +0900

    [실습] Off Canvas 메뉴 구현 과정 3
    - 어떤 것을 상태로 할 것인가? (상태 설정)
    - 조건에 따라 코드의 실행 처리

--- practices/01-off-canvas/finished/scripts/main.js
+++ practices/01-off-canvas/finished/scripts/main.js
@@ -3,14 +3,18 @@
 // --------------------------------------------------------------------------
 
 // 1. 버튼에 이벤트 리스너를 추가합니다.
+const body = document.body
 const siteContainer = document.querySelector('.site-container')
 const toggleOffsiteButton = siteContainer.querySelector('button')
+const OFFSITE_OPEN_CLASSNAME = 'offsite-is-open'
 
 toggleOffsiteButton.addEventListener('click', () => {
   // 2. 버튼을 클릭하면 화면을 오른쪽으로 밀어줍니다.
-  
+  if (!body.classList.contains(OFFSITE_OPEN_CLASSNAME)) {
+    //    <body> 요소에 offsite-is-open 클래스 이름을 추가한다.
+    body.classList.add(OFFSITE_OPEN_CLASSNAME)
+  } else {
+    // 3. 버튼을 다시 클릭하면 화면을 왼쪽으로 밀어 넣습니다.
+    body.classList.remove(OFFSITE_OPEN_CLASSNAME)
+  }
 })
-
-
-
-// 3. 버튼을 다시 클릭하면 화면을 왼쪽으로 밀어 넣습니다.
