594f89bf389b9621ef4fd9a9d91faa87755140d8
yamoo9 <yamoo9@naver.com>
2025-06-17 09:47:56 +0900

    [학습] 이벤트 헬퍼(유틸) 함수
    - helper functions
    - util functions

--- assets/scripts/learn/29-custom-callback.js
+++ assets/scripts/learn/29-custom-callback.js
@@ -1,5 +1,49 @@
+const body = document.body
 const button1 = document.querySelector('.button-1')
 const button2 = document.querySelector('.button-2')
+const button3 = document.querySelector('.button-3')
+const button4 = document.querySelector('.button-4')
+const button5 = document.querySelector('.button-5')
+
+// Mouse Event
+// - click
+// - dblclick
+// - mousedown
+// - mousemove
+// - mouseup
+// - mouseenter(element, listener)
+// - mouseleave(element, listener)
+
+const handleActive = () => {
+  body.classList.add('is-active')
+  button1.classList.add('is-active')
+  button2.classList.add('is-active')
+  button3.classList.add('is-active')
+  button4.classList.add('is-active')
+  button5.classList.add('is-active')
+}
+
+const handleInactive = () => {
+  body.classList.remove('is-active')
+  button1.classList.remove('is-active')
+  button2.classList.remove('is-active')
+  button3.classList.remove('is-active')
+  button4.classList.remove('is-active')
+  button5.classList.remove('is-active')
+}
+
+mouseenter(button1, handleActive)
+mouseleave(button1, handleInactive)
+mouseenter(button2, handleActive)
+mouseleave(button2, handleInactive)
+mouseenter(button3, handleActive)
+mouseleave(button3, handleInactive)
+mouseenter(button4, handleActive)
+mouseleave(button4, handleInactive)
+mouseenter(button5, handleActive)
+mouseleave(button5, handleInactive)
+
+// --------------------------------------------------------------------------
 
 click(document.querySelector('.button-1'), () => console.log('button 1'))
 click(document.querySelector('.button-2'), () => console.log('button 2'))
@@ -9,10 +53,6 @@ const handleClick = () => console.log('clicked button')
 
 // click(button1, handleClick)
 // click(button2, handleClick)
-
-function click(element, listener) {
-  element.addEventListener('click', listener)
-}
 
 // --------------------------------------------------------------------------
 
---
+++ assets/scripts/utils/events.js
@@ -0,0 +1,11 @@
+function click(element, listener) {
+  element.addEventListener('click', listener)
+}
+
+function mouseenter(element, listener) {
+  element.addEventListener('mouseenter', listener)
+}
+
+function mouseleave(element, listener) {
+  element.addEventListener('mouseleave', listener)
+}
