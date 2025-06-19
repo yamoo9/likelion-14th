b19674ab25681281f948bba2cfb04c18c49dca93
yamoo9 <yamoo9@naver.com>
2025-06-19 14:35:38 +0900

    [학습] 루프(Loop) 실습

--- assets/scripts/learn/33-loop.js
+++ assets/scripts/learn/33-loop.js
@@ -436,4 +436,73 @@
   // for ... of 문 (객체에서 활용하지 않음)
 
 
+})
+
+// 실습
+;(() => {
+  // 컴퓨터 프로그래밍의 기본 구조 중 하나인 루프에 숙달하기 위한 연습을 진행하세요.
+
+  // 1. for 문을 사용해 `1`부터 `100`까지의 숫자 중, 짝수만 출력
+  //   for (let i = 1, l = 100; i<=l; i++) {
+  //     if(i % 2 === 0) console.log(i)
+  //   }
+  //   for (let i = 0, l = 100; i<=l; i += 2) {
+  //     if(i > 0) console.log(i)
+  //   }
+
+  // 2. while 문을 사용해 숫자 `10`부터 `1`까지 역순으로 출력
+  // {
+  //   let i = 10
+
+  //   while(i > 0) {
+  //     console.log(i) // 10, 
+  //     i = i - 1 // 9
+  //   }
+  // }
+
+  // 3. for … of 문을 사용해 다음 배열의 과일 출력
+  const fruits = ['사과', '바나나', '귤', '복숭아']
+
+  // for (const fruit of fruits) {
+  //   console.log(fruit)
+  // }
+
+  // 4. do … while 문을 사용해 입력된 비밀번호(`'3001'`) 매칭 검토 후 결과 출력
+  // const attempts = ['0124', '8291', '3001', '9073']
+  // let i = 0, input
+
+  // do {
+  //   input = attempts[i] // '0124', '8291', '3001', ...
+
+  //   console.log('사용자 입력 시도 = '+ (i + 1) +' / 입력 값 =', input)
+
+  //   if (input === '3001') {
+  //     console.log('사용자 입력 값이 비밀번호와 일치합니다.')
+  //     break
+  //   }
+
+  //   i = i + 1 // 1, 2
+  // } while(i < attempts.length)
+
+  // 5. for … in 문을 사용해 객체의 key, value 출력
+  const student = {
+    name: '선호',
+    grade: 'A',
+    age: 22,
+  }
+
+  // for (const key in student) {
+  //   const value = student[key]
+  //   console.log(key, value)
+  // }
+
+  // 6. `1`부터 `20`까지 반복
+  //     1. `5`부터 `10`까지는 건너띄고 나머지 출력
+  //     2. `17`이 되면 반복 종료
+  // for (let i = 1; i<=20; i++) {
+  //   if (i >= 5 && i <= 10) continue
+  //   if (i === 17) break
+  //   console.log(i)
+  // }
+
 })()
