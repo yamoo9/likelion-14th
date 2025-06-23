d0b3f3940f1edb52c5c60258a7f51fad409654d6
yamoo9 <yamoo9@naver.com>
2025-06-23 10:17:27 +0900

    [학습] HTMLCollection vs. NodeList
    - Live Collection (HTMLCollection)
    - Static Collection (NodeList)

--- assets/scripts/learn/35-select-multiple-element.js
+++ assets/scripts/learn/35-select-multiple-element.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 ;(() => {
 
@@ -101,12 +102,36 @@
 
 })
 
-
 ;(() => {
   const paras = document.querySelectorAll('.life-tips p')
-  console.log(paras)
+  // console.log(paras)
 
-  console.log(paras[0] === paras.item(0))
-  console.log(paras[1] === paras.item(1))
-  console.log(paras[2] === paras.item(2))
+  // console.log(paras[0] === paras.item(0))
+  // console.log(paras[1] === paras.item(1))
+  // console.log(paras[2] === paras.item(2))
+})()
+
+// HTMLCollection vs. NodeList
+;(() => {
+  console.group('DOM 업데이트 전')
+  // HTMLCollection (Live)
+  const paras = document.getElementsByTagName('p')
+  console.log('HTMLCollection(라이브 콜렉션: 살아있는 집합) =', paras.length)
+
+  const paraClasses = document.getElementsByClassName('para')
+  console.log('HTMLCollection(라이브 콜렉션: 살아있는 집합) =',paraClasses.length)
+
+  // NodeList
+  const paragraphs = document.querySelectorAll('p')
+  console.log('NodeList(스태틱 콜렉션: 고정된 집합)', paragraphs.length)
+  console.groupEnd()
+
+  // Update DOM
+  document.querySelector('.life-tips').innerHTML += '<p class="para">씻지 않은 채소는 그대로 보관하시는 것이 좋습니다.</p>'
+
+  console.group('DOM 업데이트 후')
+  console.log('HTMLCollection(라이브 콜렉션: 살아있는 집합) =', paras.length)
+  console.log('HTMLCollection(라이브 콜렉션: 살아있는 집합) =',paraClasses.length)
+  console.log('NodeList(스태틱 콜렉션: 고정된 집합)', paragraphs.length)
+  console.groupEnd()
 })()
