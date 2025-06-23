bb59becfa8c74669a6b6b69f9cdc6ca3e391166f
yamoo9 <yamoo9@naver.com>
2025-06-23 10:38:29 +0900

    [학습] 여러 요소 선택 실습 완료

--- assets/scripts/learn/35-select-multiple-element.js
+++ assets/scripts/learn/35-select-multiple-element.js
@@ -134,4 +134,43 @@
   console.log('HTMLCollection(라이브 콜렉션: 살아있는 집합) =',paraClasses.length)
   console.log('NodeList(스태틱 콜렉션: 고정된 집합)', paragraphs.length)
   console.groupEnd()
+})
+
+// 실습
+;(() => {
+  const starWars = document.querySelector('#star-wars')
+  // console.log(starWars)
+
+  // 1. 좋은 사람(Good Guy)을 모두 선택하세요.
+  const goodGuyList = starWars.querySelectorAll('[data-type="good-guy"]')
+  // console.log(goodGuyList)
+
+  // 2. 좋은 사람(Good Guy) 모두에게 `excellent` 클래스 이름을 추가하세요.
+  goodGuyList.forEach((guy) => {
+    guy.classList.add('excellent')
+  })
+
+  // for (const guy of goodGuyList) {
+  //   guy.classList.add('excellent')
+  // }
+
+  // 3. 빌런(Villain)을 모두 선택하세요.
+  const villainList = starWars.querySelectorAll('[data-type="villain"]')
+  // console.log(villainList)
+
+  // 4. 빌런 모두에게 `naughty` 클래스 이름을 추가하세요.
+  for (let i = 0, l = villainList.length; i < l; i += 1) {
+    const villain = villainList[i]
+    villain.classList.add('naughty')
+  }
+ 
+  // 5. 모든 캐릭터(Character)를 선택하세요.
+  const allCharacters = starWars.querySelectorAll('.character')
+  console.log(allCharacters)
+
+  // 6. 모든 캐릭터에 `star-wars` 클래스 이름을 추가하세요.
+  allCharacters.forEach((c) => {
+    c.classList.add('star-wars')
+  })
+
 })()
