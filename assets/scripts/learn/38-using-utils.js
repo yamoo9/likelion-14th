3c6d0023ca7a49c7616629825419061ce80ff6a7
yamoo9 <yamoo9@naver.com>
2025-06-24 11:51:13 +0900

    [학습] 유틸리티 함수 작성 Part 2.
    - setStyle(element, propertyName, propertyValue)
    - css(element, propertyName, ?propertyValue)

--- assets/scripts/learn/38-using-utils.js
+++ assets/scripts/learn/38-using-utils.js
@@ -1,8 +1,31 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars, no-undef */
 {
   const tester = document.querySelector('h1')
   
-  const fontWeightValue = getStyle(tester, 'content', '::before')
-  console.log(fontWeightValue)
+  // 유틸리티 함수를 사용해서 요소 스타일 읽기
+  const contentValue = getStyle(tester, 'content', '::before')
+  // console.log(contentValue)
+  
+  // 유틸리티 함수를 사용해서 요소 스타일 쓰기
+  const resultElement = setStyle(tester, 'color', '#910038')
+  // console.log(tester === resultElement) // true
 
+  // 유틸리티 함수 하나를 사용해 요소 스타일 쓰기/읽기 (편의성 향상)
+  // 1. 요소에 스타일 쓰기
+  const styledElement = css(tester, 'letter-spacing', '10px')
+  // console.log(styledElement)
+
+  // 2. 요소의 스타일 읽기
+  const styledFontWeightValue = css(styledElement, 'letter-spacing')
+  // console.log(styledFontWeightValue)
+
+  // css(element, propertyName, ?propertyValue, ?pseudoElement): element | propertyValue
+  const result = css(tester, 'content', undefined, '::before')
+  console.log(result)
+
+  // const result = css({
+  //   element: tester,
+  //   pseudoElement: '::before',
+  //   propertyName: 'content',
+  // })
 }
--- assets/scripts/utils/css.js
+++ assets/scripts/utils/css.js
@@ -14,8 +14,25 @@ function getStyle(element, propertyName, pseudoElement) {
 
 // --------------------------------------------------------------------------
 // setStyle(element, propertyName, propertyValue): element
-
+function setStyle(element, propertyName, propertyValue) {
+  // 요소에 스타일 적용(쓰기)
+  element.style.setProperty(propertyName, propertyValue)
+  return element
+}
 
 // --------------------------------------------------------------------------
-// css(element, propertyName, ?propertyValue): element
-
+// css(element, propertyName, ?propertyValue, ?pseudoElement): element
+function css(element, propertyName, propertyValue, pseudoElement) {
+  // [스타일 쓰기] 속성 값이 전달되었을 때
+  if (propertyValue) {
+    // 스타일 쓰기
+    const styledElement = setStyle(element, propertyName, propertyValue)
+    return styledElement
+  } 
+  // [스타일 읽기] 속성 값이 전달되지 않았을 때
+  else {
+    // 스타일 읽기
+    const propertyValue = getStyle(element, propertyName, pseudoElement)
+    return propertyValue
+  }
+}
