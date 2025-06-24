720030f258f7334ae5a55640df064a53e15731d3
yamoo9 <yamoo9@naver.com>
2025-06-24 15:53:18 +0900

    [학습] attr 유틸리티 함수
    - 요소 속성 쓰기
    - 요소 속성 읽기

--- assets/scripts/learn/40-using-attr-function.js
+++ assets/scripts/learn/40-using-attr-function.js
@@ -1,3 +1,5 @@
+/* global attr */
+
 {
   // attr 유틸리티 함수 작성 및 활용
   // attr(element, attributeName, ?attributeValue)
@@ -11,44 +13,54 @@
   // 속성 추가(쓰기)
   // - id="heading"
   // h1.id = 'headline' // 옛날 방식
-  h1.setAttribute('id', 'heading')
+  // h1.setAttribute('id', 'heading')
+  attr(h1, 'id', 'heading')
 
   // - class="headline headline__level--1"
   // h1.className = 'headline headline__level--1' // 옛날 방식
-  h1.setAttribute('class', 'headline')
-  h1.classList.add('headline__level--1')
+  // h1.setAttribute('class', 'headline')
+  // h1.classList.add('headline__level--1')
+  attr(h1, 'class', 'headline headline__level--1')
   
   // - title="자바스크립트"
   // h1.title = '자바스크립트' // 옛날 방식
-  h1.setAttribute('title', '자바스크립트')
+  // h1.setAttribute('title', '자바스크립트')
+  attr(h1, 'title', '자바스크립트')
 
   // - data-role="headline"
-  h1.setAttribute('data-role', 'headline')
+  // h1.setAttribute('data-role', 'headline')
+  attr(h1, 'data-role', 'headline')
 
   // - data-level="1"
-  h1.setAttribute('data-level', '1')
+  // h1.setAttribute('data-level', '1')
+  attr(h1, 'data-level', '1')
 
   // h1 요소에 mouseenter 이벤트 리스너 추가
   h1.addEventListener('mouseenter', () => {  
     // 속성 읽기
     // - id
     // console.log(h1.id) // 옛날 방식
-    console.log(h1.getAttribute('id'))
+    // console.log(h1.getAttribute('id'))
+    console.log(attr(h1, 'id'))
     
     // - class
     // console.log(h1.className) // 옛날 방식
-    console.log(h1.getAttribute('class'))
-    console.log(h1.classList.value)
+    // console.log(h1.getAttribute('class'))
+    // console.log(h1.classList.value)
+    console.log(attr(h1, 'class'))
 
     // - title
     // console.log(h1.title) // 옛날 방식
-    console.log(h1.getAttribute('title'))
+    // console.log(h1.getAttribute('title'))
+    console.log(attr(h1, 'title'))
 
     // - data-role
-    console.log(h1.getAttribute('data-role'))
+    // console.log(h1.getAttribute('data-role'))
+    console.log(attr(h1, 'data-role'))
     
     // - data-level
-    console.log(h1.getAttribute('data-level'))
+    // console.log(h1.getAttribute('data-level'))
+    console.log(attr(h1, 'data-level'))
   })
 
 }
