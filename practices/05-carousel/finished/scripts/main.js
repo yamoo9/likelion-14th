c60d9ff9b27b92f68b99a2a02d3e7130f23161c9
yamoo9 <yamoo9@naver.com>
2025-06-26 14:48:34 +0900

    [학습] 캐러셀 컴포넌트 만들기 보너스 (접근성 개선 완료!)
    - 초점 이동의 논리적 순서 보장
    - tabindex="-1"
    - 사용자 탭 키 탐색으로는 접근 차단
    - JavaScript로는 접근 가능

--- practices/05-carousel/finished/scripts/main.js
+++ practices/05-carousel/finished/scripts/main.js
@@ -1521,4 +1521,153 @@
     }
   }
 
+})
+
+// 9. 키보드 접근 논리적 초점 이동 | 접근성 개선
+;(() => {
+  const carousel = document.querySelector('.carousel')
+  
+  const contentWrapper = carousel.querySelector('.carousel__contents')
+  const contents = contentWrapper.querySelectorAll('.carousel__content')
+
+  const indicatorWrapper = carousel.querySelector('.carousel__indicators')
+  const indicators = indicatorWrapper.querySelectorAll('.carousel__indicator')
+
+  const prevButton = carousel.querySelector('[aria-label^="이전"]')
+  const nextButton = carousel.querySelector('[aria-label^="다음"]')
+
+  const SELECTED_CLASSNAME = 'is-selected'
+
+  settingUpButtonHiddenStatus()
+  
+  // 로딩 시, 활성 상태(화면에 표시되는)의 
+  // 콘텐츠 내부의 링크 외 다른 링크에는 tabindex="-1" 설정
+  settingTabindexControl()
+    
+  nextButton.addEventListener('click', () => {
+    if(prevButton.hidden) prevButton.hidden = false
+
+    const selectedContent = contentWrapper.querySelector('.' + SELECTED_CLASSNAME)
+    const nextContent = selectedContent.nextElementSibling
+
+    if(!nextContent.nextElementSibling) nextButton.hidden = true
+
+    const distance = getComputedStyle(nextContent).getPropertyValue('left')
+    contentWrapper.style.setProperty('transform', 'translateX(-'+ distance +')')
+
+    selectedContent.classList.remove(SELECTED_CLASSNAME)
+    nextContent.classList.add(SELECTED_CLASSNAME)
+
+    const selectedIndicator = indicatorWrapper.querySelector('.'+SELECTED_CLASSNAME)
+    const nextIndicator = selectedIndicator.nextElementSibling
+    
+    selectedIndicator.classList.remove(SELECTED_CLASSNAME)
+    nextIndicator.classList.add(SELECTED_CLASSNAME)
+
+    settingTabindexControl()
+  })
+
+  prevButton.addEventListener('click', () => {
+    if (nextButton.hidden) nextButton.hidden = false
+
+    const selectedContent = contentWrapper.querySelector('.'+SELECTED_CLASSNAME)
+    const prevContent = selectedContent.previousElementSibling
+
+    if (!prevContent.previousElementSibling) prevButton.hidden = true
+
+    const distance = getComputedStyle(prevContent).getPropertyValue('left')
+    contentWrapper.style.setProperty('transform', 'translateX(-'+ distance +')')
+
+    selectedContent.classList.remove(SELECTED_CLASSNAME)
+    prevContent.classList.add(SELECTED_CLASSNAME)
+
+    const selectedIndicator = indicatorWrapper.querySelector('.'+SELECTED_CLASSNAME)
+    const prevIndicator = selectedIndicator.previousElementSibling
+    
+    selectedIndicator.classList.remove(SELECTED_CLASSNAME)
+
+    prevIndicator.classList.add(SELECTED_CLASSNAME)
+
+    settingTabindexControl()
+  })
+
+  for (const indicator of indicators) {
+    indicator.addEventListener('click', () => {
+      let selectedIndex
+
+      for(let i = 0, l = indicators.length; i < l; i += 1) {
+        if(indicators.item(i) === indicator) {
+          selectedIndex = i 
+          break
+        }
+      }
+
+      // selectedIndex 값이 처음(0)일 때 
+      // -> prevButton 숨긴다, nextButton 보인다
+      if (selectedIndex === 0) {
+        prevButton.hidden = true
+        nextButton.hidden = false
+      }
+
+      // selectedIndex 값이 마지막 인덱스(indicators.length -1)일 때 
+      // -> prevButton 보인다, nextButton 숨긴다
+      else if (selectedIndex === indicators.length -1) {
+        prevButton.hidden = false
+        nextButton.hidden = true
+      }
+
+      // 둘 다 아닐 때
+      // -> prevButton, nextButton 둘 다 보인다
+      else {
+        prevButton.hidden = false
+        nextButton.hidden = false
+      }
+
+
+      const activeContent = contents.item(selectedIndex) 
+      const distance = getComputedStyle(activeContent).getPropertyValue('left')
+      contentWrapper.style.setProperty('transform', 'translateX(-'+ distance +')')
+
+      const selectedContent = contentWrapper.querySelector('.'+SELECTED_CLASSNAME)
+      selectedContent.classList.remove(SELECTED_CLASSNAME)
+      activeContent.classList.add(SELECTED_CLASSNAME)
+
+      const selectedIndicator = indicatorWrapper.querySelector('.'+SELECTED_CLASSNAME)
+      selectedIndicator.classList.remove(SELECTED_CLASSNAME)
+      indicator.classList.add(SELECTED_CLASSNAME)
+
+      settingTabindexControl()
+
+    })
+  }
+
+  function settingUpButtonHiddenStatus() {
+    let selectedIndex = -1
+
+    for (let i = 0, l = contents.length; i < l; ++i) {
+      if (contents.item(i).classList.contains(SELECTED_CLASSNAME)) {
+        selectedIndex = i
+        break
+      }
+    }
+    
+    if (selectedIndex === -1) {
+      console.warn('어떤 캐러셀 콘텐츠에도 활성 상태를 나타내는 클래스 이름이 추가되지 않았습니다.')
+    } else if (selectedIndex === 0) {
+      prevButton.hidden = true
+    } else if (selectedIndex === contents.length - 1) {
+      nextButton.hidden = true
+    }
+  }
+
+  function settingTabindexControl() {
+    for (const content of contents) {
+      if (content.classList.contains(SELECTED_CLASSNAME)) {
+        content.querySelector('a').removeAttribute('tabindex')
+      } else {
+        content.querySelector('a').setAttribute('tabindex', '-1')
+      }
+    }
+  }
+
 })()
