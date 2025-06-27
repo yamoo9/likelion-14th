300b016241b45e9d1522693624403a70df832685
yamoo9 <yamoo9@naver.com>
2025-06-27 14:41:37 +0900

    [학습] 이벤트 위임 활용 주의사항
    - 중첩된 요소 처리
    - <a>, <button> 처럼 사용자와 상호작용 하는 요소의 내부
    - <svg>, <span> 요소에 이벤트가 전파되지 않도록 처리
    - CSS의 pointer-events: none 설정

--- assets/scripts/learn/47-event-delegation.js
+++ assets/scripts/learn/47-event-delegation.js
@@ -39,15 +39,14 @@
 
 // 이벤트 위임 + 이벤트 대상 확인에 따른 조건 처리
 // 단 1개의 이벤트 리스너 생성
-;(() => {
-
+;() => {
   const linkList = document.querySelector('.link-list')
-  
+
   linkList.addEventListener('click', (e) => {
     if (!e.defaultPrevented) e.preventDefault()
     const target = e.target
 
-    if(target.localName === 'a') {
+    if (target.localName === 'a') {
       // console.log('<a> 요소를 클릭했습니다.')
       console.log(target.getAttribute('href'))
     }
@@ -55,15 +54,51 @@
     // console.log(target.nodeName)  // 'A', 'LI', 'UL'
     // console.log(target.nodeName.toLowerCase())  // 'a', 'li', 'ul'
     // console.log(target.localName) // 'a', 'li', 'ul'
-    
+
     // if(target.localName === 'li') {
     //   console.log('<li> 요소를 클릭했습니다.')
     // }
-    
+
     // if(target.localName === 'ul') {
     //   console.log('<ul> 요소를 클릭했습니다.')
     // }
-
   })
+}
 
+// 이벤트 대상(event target)이 일치하는 지 검사
+// element.matches(selector)
+;(() => {
+  const linkList = document.querySelector('.link-list')
+
+  linkList.addEventListener('click', (e) => {
+    if (!e.defaultPrevented) e.preventDefault()
+    
+    const target = e.target
+
+    if (target.matches('a[href="/community"]')) {
+      console.log(target.getAttribute('href'))
+    }
+    
+  })
+})
+
+// 중첩된 요소 처리
+// <a>, <button> 요소 내부에 <svg>를 포함하는 경우
+// 1. CSS를 사용해 <a> 또는 <button> 내부의 <svg>, <span> 요소에
+//    pointer-events: none 설정을 하여 마우스 포인트 이벤트에 영향을 받지 않도록 설정
+;(() => {
+  const linkList = document.querySelector('.link-list')
+
+  linkList.addEventListener('click', (e) => {
+    if (!e.defaultPrevented) e.preventDefault()
+    
+    const target = e.target
+    
+    if (target.matches('a[href="/news"]')) {
+      console.log(target.getAttribute('href'))
+    } else {
+      console.log(target.localName)
+    }
+    
+  })
 })()
