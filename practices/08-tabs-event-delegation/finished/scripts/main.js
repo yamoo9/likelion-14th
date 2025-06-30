af45b9fcc28e8f01077658c32848d662c68333ba
yamoo9 <yamoo9@naver.com>
2025-06-30 11:51:54 +0900

    [학습] 탭 컴포넌트 이벤트 위임
    - 공통 조상에 이벤트 위임
    - Element.closest(selector) 활용해 조건 처리

--- practices/08-tabs-event-delegation/finished/scripts/main.js
+++ practices/08-tabs-event-delegation/finished/scripts/main.js
@@ -1,22 +1,43 @@
-{
+;(() => {
   const tabContainer = document.querySelector('.tabs__container')
   const tabs = Array.from(tabContainer.querySelectorAll('.tab'))
   const tabContents = Array.from(tabContainer.querySelectorAll('.tab__content'))
   const SELECTED_CLASSNAME = 'is-selected'
+  
+  let selectedIndex = -1
 
-  let selectedIndex = tabs.findIndex((tab) => tab.classList.contains(SELECTED_CLASSNAME))
+  for (let i = 0, l = tabs.length; i<l; ++i) {
+    if (tabs.at(i).classList.contains(SELECTED_CLASSNAME)) {
+      selectedIndex = i
+      break
+    }
+  }
 
-  tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
+  tabContainer.addEventListener('click', (e) => {
+    const target = e.target
+    const tabButton = target.closest('button')
+
+    if(tabButton) {
       if (selectedIndex > -1) {
         tabs.at(selectedIndex).classList.remove(SELECTED_CLASSNAME)
         tabContents.at(selectedIndex).classList.remove(SELECTED_CLASSNAME)
       }
 
-      tabs.at(index).classList.add(SELECTED_CLASSNAME)
-      tabContents.at(index).classList.add(SELECTED_CLASSNAME)
+      let activeIndex
+
+      for (let i = 0, l = tabs.length; i < l; ++i) {
+        if (tabs.at(i) === tabButton) {
+          activeIndex = i
+          break
+        }
+      }
+
+      tabs.at(activeIndex).classList.add(SELECTED_CLASSNAME)
+      tabContents.at(activeIndex).classList.add(SELECTED_CLASSNAME)
       
-      selectedIndex = index
-    })
+      selectedIndex = activeIndex
+    }
+
   })
-}
+
+})()
