31e1afc627c5967d89498acd8c0b4f696422ccc6
yamoo9 <yamoo9@naver.com>
2025-06-30 14:31:24 +0900

    [학습] CSS 트랜지션 트리거 (with JS)
    - Mouse Click Event

--- assets/scripts/learn/49-web-animations.js
+++ assets/scripts/learn/49-web-animations.js
@@ -14,13 +14,24 @@
   // Mouse Click Event + className toggling
   // 마우스로 볼(ball) 요소를 클릭하면 활성 상태로 전환
   // 마우스로 볼(ball) 요소를 다시 클릭하면 이전 상태로 전환
+  ballElement.addEventListener('click', (e) => {
+
+    e.currentTarget.classList.toggle(TRANSITION_CLASSNAME)
+    
+    // const ball = e.currentTarget
+    // if (ball.classList.contains(TRANSITION_CLASSNAME)) {
+    //   ball.classList.remove(TRANSITION_CLASSNAME)
+    // } else {
+    //   ball.classList.add(TRANSITION_CLASSNAME)
+    // }
+  })
 
   // Mouse Enter/Leave Events
-  ballElement.addEventListener('mouseenter', (e) => {
-    e.currentTarget.classList.add(TRANSITION_CLASSNAME)
-  })
-  ballElement.addEventListener('mouseleave', (e) => {
-    e.currentTarget.classList.remove(TRANSITION_CLASSNAME)
-  })
+  // ballElement.addEventListener('mouseenter', (e) => {
+  //   e.currentTarget.classList.add(TRANSITION_CLASSNAME)
+  // })
+  // ballElement.addEventListener('mouseleave', (e) => {
+  //   e.currentTarget.classList.remove(TRANSITION_CLASSNAME)
+  // })
   
 })()
