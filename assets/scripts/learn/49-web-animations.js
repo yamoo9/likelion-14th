55e5d62085646cbdf2d0683d6c406e8564096981
yamoo9 <yamoo9@naver.com>
2025-06-30 14:36:40 +0900

    [학습] CSS 트랜지션 트리거 (with JS)
    - CSS 커스텀 속성 활용
    - 개별 트랜지션 제어

--- assets/scripts/learn/49-web-animations.js
+++ assets/scripts/learn/49-web-animations.js
@@ -9,16 +9,22 @@
 ;(() => {
   
   const ballElement = document.querySelector('.ball')
-  const TRANSITION_CLASSNAME = 'is-transition'
+  // const TRANSITION_CLASSNAME = 'is-transition'
   
   // Mouse Click Event + className toggling
   // 마우스로 볼(ball) 요소를 클릭하면 활성 상태로 전환
   // 마우스로 볼(ball) 요소를 다시 클릭하면 이전 상태로 전환
   ballElement.addEventListener('click', (e) => {
 
-    e.currentTarget.classList.toggle(TRANSITION_CLASSNAME)
+    // style 속성을 사용해 속성의 상태 변경
+    // 트랜지션 트리거
+    const ball = e.currentTarget
+
+    ball.style.setProperty('--fill', '#e54d26')
+    ball.style.setProperty('--size', '100px')
+
+    // ball.classList.toggle(TRANSITION_CLASSNAME)
     
-    // const ball = e.currentTarget
     // if (ball.classList.contains(TRANSITION_CLASSNAME)) {
     //   ball.classList.remove(TRANSITION_CLASSNAME)
     // } else {
