017931f5b861d07110d76118c13ec46cf8efe024
yamoo9 <yamoo9@naver.com>
2025-06-30 11:31:30 +0900

    [실습] 아코디언 컴포넌트 이벤트 위임
    - 공통 조상을 찾아 이벤트 위임
    - Element.clossest(selector)를 활용
    - 이벤트 타겟을 찾아 조건 처리

--- practices/07-accordion-event-delegation/finished/scripts/main.js
+++ practices/07-accordion-event-delegation/finished/scripts/main.js
@@ -1,6 +1,8 @@
-{
+;(() => {
   const accordions = Array.from(document.querySelectorAll('.accordion'))
 
+  // 루프(순환)
+  // 배열의 forEach() 메서드 활용
   accordions.forEach((accordion) => {
     const button = accordion.querySelector('.accordion__header button')
     
@@ -8,4 +10,21 @@
       accordion.classList.toggle('is-open')
     })
   })
-}
+})
+
+// 이벤트 위임 방식으로 코드 개선
+;(() => {
+  const accordionContainer = document.querySelector('.accordion__container')
+
+  accordionContainer.addEventListener('click', (e) => {
+    const target = e.target
+    const button = target.closest('.accordion__header button')
+
+    if (button) {
+       button.closest('.accordion').classList.toggle('is-open')
+    } else {
+      console.log(target)
+    }
+  })
+  
+})()
