97881777cc5c0c0ba7edd23f8b525afaa2bcbcc6
yamoo9 <yamoo9@naver.com>
2025-06-30 09:32:55 +0900

    [학습] 한 번만 트리거되는 이벤트 리스너 설정
    - 동일 참조 함수를 사용해 addEventListener / removeEventListener 사용
    - 이벤트 리스너 추가 시, once: true 옵션 설정

--- assets/scripts/learn/48-remove-event-listener.js
+++ assets/scripts/learn/48-remove-event-listener.js
@@ -38,4 +38,46 @@
     }
   })
 
+})
+
+// 이벤트 리스너 추가 후, 한 번 트리거 되면, 이벤트 리스너 제거
+;(() => {
+
+  const onceButton = document.querySelector('[data-role="once-event"]')
+  
+  let clickedCount = 0
+
+  // onceButton.addEventListener('click', () => {
+  //   clickedCount += 1
+  //   console.log(clickedCount + '번 클릭했습니다.')
+  // })
+
+  // 한 번만 이벤트를 트리거 하려면?
+  // 이벤트가 트리거 되면 추가된 이벤트 리스너를 제거한다.
+  onceButton.addEventListener('click', handleOnceClick)
+
+  function handleOnceClick(e) {
+    clickedCount += 1
+    console.log(clickedCount + '번 클릭했습니다.')
+    e.currentTarget.removeEventListener('click', handleOnceClick)
+    console.log('추가된 이벤트 리스너 handleOnceClick이 제거되었습니다.')
+  }
+
+})
+
+// 이벤트 리스너 옵션(Options) 객체 설정으로 once 이벤트 구현하기
+;(() => {
+  
+  const onceButton = document.querySelector('[data-role="once-event"]')
+  
+  let clickedCount = 0
+
+  onceButton.addEventListener(
+    'click',
+    () => {
+      console.log(++clickedCount + '번 클릭했어요!')
+    },
+    { once: true },
+  )
+
 })()
