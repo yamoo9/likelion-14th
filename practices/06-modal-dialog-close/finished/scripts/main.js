d8a9ba072c03611f233e19413e846f86a3dd27dd
yamoo9 <yamoo9@naver.com>
2025-06-30 11:09:25 +0900

    [학습] 모달 다이얼로그 닫기 기능 실습 완료
    - 이벤트 위임
    - 이벤트 버블링 단계
    - 이벤트 타겟 단계
    - 이벤트 전파 중지 (권장 안함)
    - Element.closest(selector) 활용

--- practices/06-modal-dialog-close/finished/scripts/main.js
+++ practices/06-modal-dialog-close/finished/scripts/main.js
@@ -1,9 +1,23 @@
-{
+;(() => {
+  document.body.addEventListener('click', (e) => {
+    console.log(e.target.localName)
+  })
+})
+
+;(() =>{
   const body = document.body
   const modal_open_button = document.querySelector('main .modal-open-button')
 
   const modal_overlay = document.querySelector('.modal-overlay')
   const modal_close_button = modal_overlay.querySelector('.modal-close-button')
+  
+  modal_overlay.addEventListener('click', closeModalDialog)
+  
+  // const modal_dialog = modal_overlay.querySelector('.modal-dialog')
+  // modal_dialog.addEventListener('click', (e) => {
+    // e.stopPropagation()
+    // console.log('[이벤트 버블링] 이벤트 전파 중지')
+  // })
 
   modal_open_button.addEventListener('click', () => {
     body.classList.add('modal-is-open')
@@ -11,7 +25,16 @@
 
   modal_close_button.addEventListener('click', closeModalDialog)
 
-  function closeModalDialog() {
-    body.classList.remove('modal-is-open')
+  function closeModalDialog(e) {
+    const target = e.target
+
+    if (
+      !target.closest('.modal-dialog') ||
+      target === modal_close_button
+    ) {
+      body.classList.remove('modal-is-open')
+    }
+
   }
-}
+
+})()
