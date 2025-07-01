a8090e61a2ef8a6c069edcad27ac87454851ee7e
yamoo9 <yamoo9@naver.com>
2025-07-01 16:51:25 +0900

    [학습] GSAP - 배너 애니메이션 가이드

---
+++ assets/pages/13-gsap-animation/banners/animate.js
@@ -0,0 +1,31 @@
+// 배너 애니메이션을 GSAP 애니메이션 라이브러리로 구현해보세요.
+;(() => {
+  /* global gsap, GSDevTools */
+
+  // 애니메이션
+  const tl = gsap.timeline({
+    id: 'banner animation',
+    defaults: { opacity: 0, ease: 'back.out(1.4)' },
+  })
+
+  tl
+    .from('.banner', { ease: 'linear', autoAlpha: 0, })
+    .from('.banner__title :first-child', { x: 80, duration: 1 })
+    .from('.banner__title :last-child', { x: -80, duration: 1 }, '<')
+    .from('.banner__description', { y: 30 }, '-=0.5') 
+    .from('.banner__link', { y: 40, }, '-=0.4')
+    .from('#items > g', { scale: 0, transformOrigin: 'center', stagger: 0.1 }, '-=0.2')
+
+  // 플러그인
+  gsap.registerPlugin(GSDevTools)
+  GSDevTools.create({ animation: tl, loop: true, id: 'main' })
+
+  // 이벤트 핸들링
+  document.querySelector('.banner')
+  .addEventListener('click', () => {
+      alert('devtools kill!!!!')
+      // 개발 도구 GSDevTools 인스턴스 제거
+      GSDevTools.getById('main').kill()
+    })
+
+})()
