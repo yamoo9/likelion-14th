6921317e9fa3424d9a5083673425ac66aef29a6e
yamoo9 <yamoo9@naver.com>
2025-07-02 11:50:06 +0900

    [학습] 아코디언(Accordion) JavaScript 애니메이션 적용 후 접근성 고려
    - 탭 콘텐츠 내부의 초점 이동 가능한 요소 탐색
    - tabindex="-1" 설정으로 초점 이동 제한
    - 콘텐츠가 열려서 화면에 표시되면 tabindex속성 값을 0으로 설정

--- practices/13-accordion-animation/finished/scripts/main.js
+++ practices/13-accordion-animation/finished/scripts/main.js
@@ -1,7 +1,10 @@
 {
   const OPEN_CLASSNAME = 'is-open'
   const accordionContainer = document.querySelector('.accordion__container')
-  
+  const accordionContentLinks = accordionContainer.querySelectorAll('.accordion__content a')
+
+  accordionContentLinks.forEach((link) => link.setAttribute('tabindex', '-1'))
+
   accordionContainer.addEventListener('click', (e) => {
     const button = e.target.closest('button')
     
@@ -9,19 +12,24 @@
       const accordion = button.closest('.accordion')
       // 아코디언 이너의 계산된 높이 값 브라우저로부터 가져오기
       const accordionContent = accordion.querySelector('.accordion__content')
+      const accordionLink = accordionContent.querySelector('a')
       const accordionInner = accordionContent.firstElementChild
       const height = accordionInner.getBoundingClientRect().height
+      
 
       // 열린 / 닫힌 상태에 따른 조건 분기
       // 아코디언이 열리지 않은 상태라면
       if (!accordion.classList.contains(OPEN_CLASSNAME)) {
         // 열린 상태가 되기 위해 블록 사이즈(높이) 계산된 높이 값으로 설정
         accordionContent.style.setProperty('block-size', height + 'px')
+        // accordionLink.removeAttribute('tabindex')
+        accordionLink.setAttribute('tabindex', '0')
       } 
       // 아코디언이 열린 상태라면
       else {
         // 닫힌 상태가 되기 위해 블록 사이즈(높이) 0으로 설정
         accordionContent.style.setProperty('block-size', 0)
+        accordionLink.setAttribute('tabindex', '-1')
       }
 
       accordion.classList.toggle(OPEN_CLASSNAME)
