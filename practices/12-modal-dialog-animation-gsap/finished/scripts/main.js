f8609f34a7808e0ffc8df6865e3988612809da7d
yamoo9 <yamoo9@naver.com>
2025-07-02 10:51:01 +0900

    [학습] 모달 다이얼로그(Modal Dialog) 애니메이션 적용 (with GSAP)

--- practices/12-modal-dialog-animation-gsap/finished/scripts/main.js
+++ practices/12-modal-dialog-animation-gsap/finished/scripts/main.js
@@ -1,13 +1,19 @@
+/* global gsap */
+
 {
   const body = document.body
   const modal_open_button = document.querySelector('main .modal-open-button')
   const modal_overlay = document.querySelector('.modal-overlay')
+  const waveHand = modal_overlay.querySelector('.wave-hand')
+
+  animateRisingUp()
 
   modal_open_button.addEventListener('click', openModalDialog)
   modal_overlay.addEventListener('click', closeModalDialog)
 
   function openModalDialog() {
     body.classList.add('modal-is-open')
+    animateWaveHand()
   }
 
   function closeModalDialog(e) {
@@ -17,5 +23,35 @@
       !target.closest('.modal-dialog') || 
       target.closest('.modal-close-button')
     ) body.classList.remove('modal-is-open')
+  }
+
+  function animateRisingUp() {
+    gsap.from('.point-hand', {
+      y: 50,
+      opacity: 0,
+      duration: 1.25,
+      ease: 'back.inOut(4)',
+      repeat: -1,
+      repeatDelay: 0.4,
+    })
+  }
+
+  function animateWaveHand() {
+    gsap
+      .timeline({ defaults: { duration: 0.15 } })
+      .from(waveHand, {
+        scale: 0,
+        opacity: 0,
+        duration: 0.4,
+        delay: 0.3,
+        ease: 'back.out(3)',
+      })
+      .set(waveHand, { transformOrigin: 'center bottom' })
+      .to(waveHand, { rotation: 15 })
+      .to(waveHand, { rotation: -15 })
+      .to(waveHand, { rotation: 15 })
+      .to(waveHand, { rotation: -15 })
+      .to(waveHand, { rotation: 15 })
+      .to(waveHand, { rotation: 0 })
   }
 }
