1cfa5e8e6cc78e158a8cc76a5ca7502d78ac76f1
yamoo9 <yamoo9@naver.com>
2025-07-02 11:46:16 +0900

    [학습] 아코디언(Accordion) JavaScript 애니메이션 적용
    - interpolate-size 브라우저 호환성 문제
    - 클라이언트 요구 사항 충족을 위해
    - JavaScript 애니메이션 적용
    - HTML 구조 변겨
    - 레이아웃 스타일 조정
    - 요소의 getBoundingClientRect().height
    - JS로 트랜지션되도록 구현

--- practices/13-accordion-animation/finished/scripts/main.js
+++ practices/13-accordion-animation/finished/scripts/main.js
@@ -1,4 +1,5 @@
 {
+  const OPEN_CLASSNAME = 'is-open'
   const accordionContainer = document.querySelector('.accordion__container')
   
   accordionContainer.addEventListener('click', (e) => {
@@ -6,7 +7,24 @@
     
     if (button) {
       const accordion = button.closest('.accordion')
-      accordion.classList.toggle('is-open')
+      // 아코디언 이너의 계산된 높이 값 브라우저로부터 가져오기
+      const accordionContent = accordion.querySelector('.accordion__content')
+      const accordionInner = accordionContent.firstElementChild
+      const height = accordionInner.getBoundingClientRect().height
+
+      // 열린 / 닫힌 상태에 따른 조건 분기
+      // 아코디언이 열리지 않은 상태라면
+      if (!accordion.classList.contains(OPEN_CLASSNAME)) {
+        // 열린 상태가 되기 위해 블록 사이즈(높이) 계산된 높이 값으로 설정
+        accordionContent.style.setProperty('block-size', height + 'px')
+      } 
+      // 아코디언이 열린 상태라면
+      else {
+        // 닫힌 상태가 되기 위해 블록 사이즈(높이) 0으로 설정
+        accordionContent.style.setProperty('block-size', 0)
+      }
+
+      accordion.classList.toggle(OPEN_CLASSNAME)
     }
   })
 }
