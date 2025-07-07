6295fee394ffadfc6dea1c3a4250103c337bd506
yamoo9 <yamoo9@naver.com>
2025-07-07 14:42:12 +0900

    [학습] 객체 속성 순환
    - UI 템플릿 구성
    - 테이블 템플릿 작성
    - 객체의 속성 순환
    - Object.entries()
    - map() + join()
    - reduce()

--- assets/scripts/learn/61-loop-object-properties.js
+++ assets/scripts/learn/61-loop-object-properties.js
@@ -101,5 +101,120 @@
     console.log(value)
   })
 
-})()
+})
 
+// 실습 1. 언어 이름 출력
+// 다음 객체가 주어졌을 때 각 프로그래밍 언어의 한글 이름을 모두 콘솔 패널에 출력합니다.
+;(() => {
+  
+  const 프로그래밍_언어 = {
+    자바스크립트: 'JavaScript',
+    파이썬: 'Python',
+    루비: 'Ruby',
+    고: 'Golang',
+  }
+
+  // for...of
+  for (const 언어 of Object.keys(프로그래밍_언어)) console.log(언어)
+
+  // forEach()
+  Object.keys(프로그래밍_언어).forEach((언어) => console.log(언어))
+
+})
+
+// 실습 2. 점수의 총 합계 출력
+// 다음 객체가 주어졌을 때, 점수의 총 합계를 콘솔 패널에 출력합니다.
+;(() => {
+  const 점수 = {
+    수학: 90,
+    과학: 85,
+    영어: 92,
+    국어: 100,
+  }
+
+  // for...of
+  {
+    let totalScore = 0
+    for (const score of Object.values(점수)) totalScore += score
+    console.log(totalScore)
+  }
+
+  // forEach()
+  {
+    let totalScore = 0
+    Object.values(점수).forEach((score) => totalScore += score)
+    console.log(totalScore)
+  }
+
+  // reduce()
+  {
+    const totalScore = Object.values(점수).reduce((totalScore, score) => totalScore + score, 0)
+    console.log(totalScore)
+  }
+
+})
+
+// 실습 3. 객체의 키, 값 표로 출력
+// 다음 객체의 키, 값을 표(table)로 템플릿 스트링으로 출력합니다.
+;(() => {
+
+  const 프로필 = {
+    이름: '하영',
+    성별: '여성',
+    나이: 26,
+    거주지: '서울',
+    직업: '공무원',
+  }
+
+  console.log(Object.entries(프로필))
+
+  // for...of → 문이므로 값을 반환 ❌
+  // forEach() → 식이지만 undefined 반환 ❌
+  // map() + join() → 식으로 값을 반환 ✅
+  // reduce() → 식으로 값을 반환 ✅
+
+  // map() + join() 코드 예시
+  // const tableTemplate = `
+  //   <table id="practice">
+  //     <caption>프로필 정보</caption>
+  //     <tbody>
+  //       ${Object
+  //         .entries(프로필)
+  //         .map(([key, value]) => `
+  //           <tr>
+  //             <th scope="row">${key}</th>
+  //             <td>${value}</td>
+  //           </tr>
+  //         `)
+  //         .join('')
+  //       }
+  //     </tbody>
+  //   </table>
+  // `
+
+  // reduce() 코드 예시
+  const tableTemplate = `
+    <table id="practice">
+      <caption>프로필 정보</caption>
+      <tbody>
+        ${createTableRowsTemplate()}
+      </tbody>
+    </table>
+  `
+
+  function createTableRowsTemplate() {
+    return Object
+          .entries(프로필)
+          .reduce((templateString, [key, value]) => {
+            const tableRowTemplate = `
+              <tr>
+                <th scope="row">${key}</th>
+                <td>${value}</td>
+              </tr>
+            `
+            return `${templateString}${tableRowTemplate}`
+          }, '')
+  }
+
+  console.log(tableTemplate)
+})()
