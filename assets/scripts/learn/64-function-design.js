4400ae50beaf2f7cf300d5fb6bd1cacfd5136489
yamoo9 <yamoo9@naver.com>
2025-07-08 11:43:07 +0900

    [학습] 명확한 목적에 맞는 함수 설계
    - 적절한 함수 이름
    - 명확하기 이해 가능한 주석 (JSDoc)

--- assets/scripts/learn/64-function-design.js
+++ assets/scripts/learn/64-function-design.js
@@ -56,4 +56,57 @@
 
   const result = map(numbers, (n) => n ** 2)
   console.log(result) // [4, 81, 441, 1156]
+})
+
+// 적절한 함수 이름과 이해하기 쉬운 주석 설정
+;(() => {
+  
+  // JSDoc 마크업 언어 사용 ----------------------------------------------------------
+  /**
+   * 함수의 목적
+   * 함수의 매개변수
+   * 함수의 반환값
+   */
+
+  /**
+   * 집합에서 특정 항목을 걸러주는 기능
+   * 
+   * @param {Array} array 걸러내야 할 원본 배열
+   * @param {Function} callback 집합을 순환할 때마다 실행되는 콜백 함수
+   * @returns {Array} 특정 항목이 걸러진 새 배열
+   */
+  function filter(array, callback) {
+    const filteredArray = []
+    for(let index = 0, l = array.length; index < l; index++) {
+      const item = array[index]
+      if(callback(item, index, array)) {
+        filteredArray.push(item)
+      }
+    }
+
+    return filteredArray
+  }
+
+  /**
+   * 집합의 모든 항목을 변형하는 기능
+   * 
+   * @param {Array} likeArray 변형할 항목을 포함하는 원본 배열
+   * @param {Function} callback 집합을 순환할 때마다 실행되는 콜백 함수
+   * @returns {Array} 변형된 모든 항목을 포함하는 새 배열
+   */
+  function map(likeArray, callback) {
+    const resultArray = []
+    for(let index = 0; index < likeArray.length; index += 1) {
+      const item = likeArray[index]
+      const mutatedItem = callback(item, index, likeArray)
+      resultArray.push(mutatedItem)
+    }
+    return resultArray
+  }
+
+  const filteredArray = filter([2, 3, 4, 5], (n) => n > 3 && n < 5)
+  console.log(filteredArray)
+
+  console.log(map(['one', 'two'], (item, index) => `${index}-${item}`))
+
 })()
