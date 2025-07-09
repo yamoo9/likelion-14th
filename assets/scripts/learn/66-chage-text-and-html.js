a371b9397c933ac42cad46ae952f25e7e56a65ef
yamoo9 <yamoo9@naver.com>
2025-07-09 09:47:45 +0900

    [학습] 텍스트 및 HTML 콘텐츠 변경
    - 텍스트 콘텐츠 변경 시, textContent
    - HTML 콘텐츠 변경 시, innerHTML

---
+++ assets/scripts/learn/66-chage-text-and-html.js
@@ -0,0 +1,47 @@
+// --------------------------------------------------------------------------
+// 📌 텍스트 및 HTML 콘텐츠 변경
+// --------------------------------------------------------------------------
+
+;(() => {
+  
+  // 텍스트 콘텐츠 변경
+  const element = document.querySelector('h1')
+  element.textContent = '자바스크립트'
+  
+  // HTML 콘텐츠 읽기
+  console.log(element.innerHTML)
+
+  // HTML 콘텐츠 변경
+  // 기존 데이터 모두 덮어쓰기
+  element.innerHTML = `
+    <span>자</span>
+    <span>바</span>
+    <span>스</span>
+    <span>크</span>
+    <span>립</span>
+    <span>트</span>
+  `.trim()
+
+  // 기존 HTML 콘텐츠에 추가
+  
+  // 뒤에 추가
+  // element.innerHTML = element.innerHTML + '<em>!</em>'
+  element.innerHTML += '<em>!</em>'
+
+  // 앞에 추가
+  element.innerHTML = ' — ' + element.innerHTML
+
+  // HTML 콘텐츠 읽기
+  console.log(element.innerHTML)
+
+  // 마운트 요소 내부에 목록 동적 추가
+  document.querySelector('.mount')
+    .innerHTML = /* html */`
+      <ul class="italian-menu">
+        <li>피자</li>
+        <li>파스타</li>
+        <li>리조또</li>
+      </ul>
+    `
+
+})()
