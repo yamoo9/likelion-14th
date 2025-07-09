e256c349e1a7e17afd7f108fa717c84ecfaf65d9
yamoo9 <yamoo9@naver.com>
2025-07-09 16:16:25 +0900

    [학습] 요소를 DOM에서 제거
    - ParentNode.removeChild(ChildNode)
    - Element.remove()

---
+++ assets/scripts/learn/69-remove-element-from-dom.js
@@ -0,0 +1,48 @@
+// --------------------------------------------------------------------------
+// 📌 DOM에서 요소 제거
+// --------------------------------------------------------------------------
+
+;(() => {
+
+  // 동적으로 요소들 추가
+  const section = document.querySelector('section')
+  section.insertAdjacentHTML('beforeend', `
+    <ul class="members">
+      <li>이지아</li>
+      <li>박현일</li>
+      <li>최상준</li>
+      <li>하준경</li>
+    </ul>  
+  `)
+
+  // 요소 제거
+  const willRemoveElement = section.querySelector('.members li:nth-child(3)')
+  
+  // 타이머
+  setTimeout(() => {
+    // parentElement.removeChild(childElement)
+    const parent = willRemoveElement.parentElement
+    const removedElement = parent.removeChild(willRemoveElement)
+    // console.log(removedElement)
+
+    // 제거된 요소를 다시 하준경 뒤에
+    // parent.append(removedElement)
+
+    // 제거된 요소를 다시 이지아 앞에
+    // parent.prepend(removedElement)
+    
+    // 제거된 요소를 다시 박현일 뒤에
+    // const children = [...parent.children]
+    // children.at(-1).before(removedElement)
+    
+    // 제거된 요소를 다시 박현일 앞에
+    const [, park] = parent.children
+    park.before(removedElement)
+
+
+    // element.remove()
+    // const returnValue2 = willRemoveElement.remove()
+    // console.log(returnValue2)
+  }, 1500)
+
+})()
