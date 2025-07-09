28307855f352edbf3eabce8e21c4905e99f9c039
yamoo9 <yamoo9@naver.com>
2025-07-09 10:06:33 +0900

    [학습] 텍스트 콘텐츠 → 배열화 → HTML 코드 생성 → 애니메이션

--- assets/scripts/learn/66-chage-text-and-html.js
+++ assets/scripts/learn/66-chage-text-and-html.js
@@ -2,6 +2,8 @@
 // 📌 텍스트 및 HTML 콘텐츠 변경
 // --------------------------------------------------------------------------
 
+/* global gsap */
+
 ;(() => {
   
   // 텍스트 콘텐츠 변경
@@ -9,39 +11,55 @@
   element.textContent = '자바스크립트'
   
   // HTML 콘텐츠 읽기
-  console.log(element.innerHTML)
+  // console.log(element.innerHTML)
 
   // HTML 콘텐츠 변경
   // 기존 데이터 모두 덮어쓰기
-  element.innerHTML = `
-    <span>자</span>
-    <span>바</span>
-    <span>스</span>
-    <span>크</span>
-    <span>립</span>
-    <span>트</span>
-  `.trim()
+  // element.innerHTML = `
+  //   <span>자</span>
+  //   <span>바</span>
+  //   <span>스</span>
+  //   <span>크</span>
+  //   <span>립</span>
+  //   <span>트</span>
+  // `.trim()
+  // 읽어온 텍스트 콘텐츠를 배열로 변환
+  element.innerHTML = element.textContent.split('').reduce((acc, cur) => {
+    return acc + `<span>${cur}</span>`
+  }, '')
 
   // 기존 HTML 콘텐츠에 추가
   
   // 뒤에 추가
   // element.innerHTML = element.innerHTML + '<em>!</em>'
-  element.innerHTML += '<em>!</em>'
+  // element.innerHTML += '<em>!</em>'
 
   // 앞에 추가
-  element.innerHTML = ' — ' + element.innerHTML
+  // element.innerHTML = ' — ' + element.innerHTML
 
   // HTML 콘텐츠 읽기
-  console.log(element.innerHTML)
+  // console.log(element.innerHTML)
+
+  // GSAP 애니메이션
+  gsap.from('h1 span', {
+    y: 50,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 1.4,
+    ease: 'back.inOut(7)',
+  })
 
   // 마운트 요소 내부에 목록 동적 추가
-  document.querySelector('.mount')
-    .innerHTML = /* html */`
-      <ul class="italian-menu">
-        <li>피자</li>
-        <li>파스타</li>
-        <li>리조또</li>
-      </ul>
-    `
+  const mount = document.querySelector('.mount')
+  
+  mount.innerHTML = /* html */`
+    <ul class="italian-menu">
+      <li>피자</li>
+      <li>파스타</li>
+      <li>리조또</li>
+    </ul>
+  `
+
+  console.log(mount.outerHTML)
 
 })()
