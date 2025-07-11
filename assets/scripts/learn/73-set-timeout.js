8434313c644b22d469e0e4307d4de30d07389e25
yamoo9 <yamoo9@naver.com>
2025-07-11 13:44:07 +0900

    [학습] 재귀(再歸) 호출
    - 함수 자신을 다시 호출하는 문제 해결 방법
    - 리커젼(recursion)

--- assets/scripts/learn/73-set-timeout.js
+++ assets/scripts/learn/73-set-timeout.js
@@ -39,4 +39,52 @@
   clearTimeout(timeoutId2)
   console.log('timeoutId2 타이머 해제')
 
+})
+
+// 리커젼 (recursion, 재귀(再歸): 자신을 다시 호출)
+;(() => {
+
+  let count = 10
+
+  countDown()
+
+  function countDown() {
+    const timeoutId = setTimeout(() => {
+      console.log(count)
+
+      // 재귀 호출을 중단하기 위한 조건 설정
+      if (count === 0) {
+        console.log('타임아웃 해제')
+        clearTimeout(timeoutId)
+        return
+      }
+      
+      count -= 1
+
+      // countDown 함수 자신을 다시 호출할 수 있다면?
+      // console.log(countDown) // 함수 안에서 함수 자신에 접근 가능
+      // 자신(함수)을 다시 호출 것도 가능
+      countDown()
+    }, 1000)
+  }
+
+  // setTimeout(() => {
+  //   countDown()
+  // }, 1000)
+
+  // 반복문으로 카운트 다운
+  // for(let i = count; i > 0; i -= 1) {
+  //   countDown()
+  // }
+
+})()
+
+// 쓰로틀링 (Throttling)
+;(() => {
+  
+})()
+
+// 디바운싱 (Debouncing)
+;(() => {
+  
 })()
