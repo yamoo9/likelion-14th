c2a828a2d37b9609672f3db3cfe67b6cda45d73a
yamoo9 <yamoo9@naver.com>
2025-07-11 11:50:01 +0900

    [실습] 할 일 목록 만들기 4
    - 할 일 제거 기능 구현
    - 이벤트 위임 방식 활용
    - 할 일 추가 안내 사항 표시

--- practices/32-to-do-list/finished/scripts/main.js
+++ practices/32-to-do-list/finished/scripts/main.js
@@ -5,21 +5,6 @@
   // 문서에서 폼 요소 찾기
   const todoListForm = document.querySelector('.todolist')
   const todoList = todoListForm.querySelector('.todolist__tasks')
-
-  // 할 일 제거 버튼 찾기
-  const removeButtons = todoList.querySelectorAll('button')
-  
-  // 찾은 제거 버튼(들) 순환
-  removeButtons.forEach((button) => {
-    // 할 일 제거 버튼 click 이벤트 리스너 추가 (개별)
-    button.addEventListener('click', (e) => {
-      const taskElement = e.currentTarget.closest('.task')
-      // 제거 방법 1
-      // taskElement.parentElement.removeChild(taskElement)
-      // 제거 방법 2
-      taskElement.remove()
-    })
-  })
   
   // 폼 요소에 submit 이벤트 리스너 추가
   todoListForm.addEventListener('submit', (e) => {
@@ -53,6 +38,39 @@
     
   })
 
+  // 이벤트 위임 방식을 사용해
+  // 동적 생성된 할 일 요소의 삭제 기능이 처리되도록 구현
+  todoList.addEventListener('click', ({ currentTarget: list, target }) => {
+    const removeButton = target.closest('[aria-label="할 일 삭제"]')
+    
+    if (!removeButton) return
+    
+    // 삭제 기능 작동 (할 일 요소 제거)
+    const willRemoveTaskElement = removeButton.closest('.task')
+    // willRemoveTaskElement.parentElement.removeChild(willRemoveTaskElement)
+    willRemoveTaskElement.remove()
+
+    // 할 일 목록 내부에 아무런 자식 요소가 없다면?
+    // <ul> 요소 내부에 공백이 없도록 구성
+    // 사용자에게 새 할 일 추가에 관한 안내 사항 표시
+    if(list.children.length === 0) list.innerHTML = ''
+  })  
+
+  // 할 일 제거 버튼 찾기
+  // const removeButtons = todoList.querySelectorAll('button')
+  
+  // 찾은 제거 버튼(들) 순환
+  // removeButtons.forEach((button) => {
+  //   // 할 일 제거 버튼 click 이벤트 리스너 추가 (개별)
+  //   button.addEventListener('click', (e) => {
+  //     const taskElement = e.currentTarget.closest('.task')
+  //     // 제거 방법 1
+  //     // taskElement.parentElement.removeChild(taskElement)
+  //     // 제거 방법 2
+  //     taskElement.remove()
+  //   })
+  // })
+
   // 새로운 할 일 생성 함수
   function createTask(content) {
     // 생성할 할 일 요소
@@ -82,9 +100,6 @@
         </svg>
       </button>
     `)
-
-    // 생성된 <li> 안에서 제거 버튼을 찾아서
-    // 버튼에 할 일 요소를 삭제하는 기능을 추가
 
     // 생성된 새 할 일 요소 반환
     return taskElement
