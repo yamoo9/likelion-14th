5be027b12da1139160bb38c134265f54d89b17d4
yamoo9 <yamoo9@naver.com>
2025-07-16 14:19:14 +0900

    [학습] 이미지 업로드 기능 구현 3
    - 웹 스토리지를 활용해 데이터 쓰기 및 읽기
    - 업로드 후, 화면에 렌더링

--- assets/scripts/learn/85-async-js-file-upload.js
+++ assets/scripts/learn/85-async-js-file-upload.js
@@ -1,18 +1,22 @@
-/* global axios */
+/* global axios, DOMPurify */
 
 // --------------------------------------------------------------------------
 // 📌 파일(이미지) 업로드
 // --------------------------------------------------------------------------
 
+const IMGBB_API = 'https://api.imgbb.com/1/upload'
+const IMGBB_API_KEY = 'IMGBB_API_KEY'
+const IMGBB_EXPIRATION = 60 * 10 * 6 // 60분 만료 (삭제)
+const STORAGE_KEY = '@imgbb'
+
 // 멀티 이미지 업로드 기능 구현
 ;(() => {
   
-  const IMGBB_API = 'https://api.imgbb.com/1/upload'
-  const IMGBB_API_KEY = 'IMGBB_API_KEY'
-  const IMGBB_EXPIRATION = 60 * 10 * 6 // 60분 만료 (삭제)
 
   const uploadForm = document.querySelector('[data-id="upload-form"]')
   const fileInput = uploadForm.querySelector('input[type="file"')
+
+  renderImagesFromStorage()
   
   fileInput.addEventListener('change', (e) => {
     const fileInput = e.currentTarget
@@ -34,10 +38,19 @@
     for (const file of form.image.files) {
       // 이미지 포멧 파일이 아니면 건너띄기
       if (!file.type.startsWith('image/')) continue
+
       // 이미지 포멧 파일만 업로드 (비동기 요청)
       fetchUploadImage(file)
-        .then(({ data }) => {
-          console.log(data.url)
+        .then(({ data: { data: { url } } }) => {
+          
+          // 서버 측의 데이터베이스에 기억할 수 없는 상황이라면?
+          // 클라이언트 측의 스토리지(Storage)에 기억
+          const storage = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? []
+          // console.log(url)
+          // console.log('가져왔을 때', storage.length)
+          storage.push(url)
+          // console.log('추가했을 때', storage.length)
+          localStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
         })
         .catch((error) => {
           console.error(error)
@@ -47,6 +60,9 @@
     // 폼 초기화
     deleteThumbnails()
     form.reset()
+
+    // 이미지 렌더링
+    renderImagesFromStorage()
   })
 
   function fetchUploadImage(file) {
@@ -103,6 +119,7 @@
     uploadForm.querySelector('.thumbnails')?.remove()
   }
 
+  // eslint-disable-next-line no-unused-vars
   function learnFormData(form) {
     // 폼 요소의 데이터를 가져오기 위해 폼 데이터 객체 생성
     const formData = new FormData()
@@ -120,6 +137,23 @@
 
     // formData -> JavaScript Object
     console.log(Object.fromEntries(formData))
+  }
+
+  // 웹 스토리지에 저장된 데이터를 불러와 화면에 렌더링
+  function renderImagesFromStorage() {
+    // 스토리지에서 데이터 읽기
+    const imageArray = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    // console.log(imageArray)
+
+    // DOM 컨테이너 요소 찾기
+    const images = document.querySelector('.images')
+    
+    // 템플릿 리터럴 구성
+    const imagesTemplate = imageArray?.reduce((template, imageURL) => {
+      return template + `<img src="${imageURL}" alt="" width="100" height="100" />`
+    }, '') ?? ''
+
+    images.innerHTML = DOMPurify.sanitize(imagesTemplate)
   }
 
 })()
